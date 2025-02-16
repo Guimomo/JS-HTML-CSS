fe7df47965c9a841b6352fc7a20fadbdc28f83c3
Guillermo Garrido <gnilleuni@gmail.com>
2025-02-15 20:06:55 -0500

    js html css

---
+++ app.js
@@ -0,0 +1,146 @@
+// const padre = document.querySelector(".container");
+// const nodo = document.createElement("h2");
+// padre.appendChild(nodo)
+
+const list = document.querySelector("#list");
+
+let item = document.createElement("li");
+let html = document.createElement("li");
+let css = document.createElement("li");
+let js = document.createElement("li");
+
+item.textContent = "Item nuevo";
+html.textContent = "HTML";
+css.textContent = "CSS";
+js.textContent = "JS";
+
+item.classList.add("item");
+html.classList.add("item");
+css.classList.add("item");
+js.classList.add("item");
+
+list.append(item, js, css, html);
+
+const card = document.querySelector(".card");
+const lista_sin_orden = document.querySelector("#list");
+const titulo = document.createElement("h2");
+titulo.textContent = "Soy el titulo más de los más";
+// card.appendChild(titulo);
+// card.insertBefore(titulo, final)
+
+const afterbegin = document.createElement("li");
+afterbegin.classList.add("item");
+afterbegin.textContent = "Al inicio de la lista ordenada";
+
+// solucion
+lista_sin_orden.insertAdjacentElement("afterend", afterbegin);
+
+const dias = [
+  {
+    id: 1,
+    name: "Lunes",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+  {
+    id: 2,
+    name: "Martes",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+  {
+    id: 3,
+    name: "Miercoles",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+  {
+    id: 4,
+    name: "Jueves",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+  {
+    id: 5,
+    name: "Viernes",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+  {
+    id: 6,
+    name: "Sabado",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+  {
+    id: 7,
+    name: "Domingo",
+    img: "https://picsum.photos/640/360",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consectetur.",
+  },
+];
+
+// dias.map(dia=>{
+//   console.log(dia.name);
+// })
+
+const $dias = document.getElementById("dias");
+
+const $fragment = document.createDocumentFragment(); //el fragment es un elemento virtual ocupara menos memoria
+
+dias.map(({name,img,paragraph})=>{
+
+  //creamos elementos
+
+  const $card = document.createElement("div");
+  const $card_header = document.createElement("div");
+  const $card_h2 = document.createElement("h2");
+  const $card_body = document.createElement("div");
+  const $card_img = document.createElement("img");
+  const $card_paragraph = document.createElement("p");
+  const $button = document.createElement("a");
+  const $button_span = document.createElement("span");
+  const $button_icon = document.createElement("i");
+
+  $card.classList.add("card");
+  $card_header.classList.add("card__header");
+  $card_h2.classList.add("card__title");
+  $card_body.classList.add("card__body");
+  $card_img.classList.add("card__img");
+  $card_paragraph.classList.add("card__paragraph");
+  $button.classList.add("button","button--outline");
+  $button_span.classList.add("button__text");
+  $button_icon.classList.add("bx","bxs-chevro-right", "button__icon");
+
+  //agregamos los valores del objeto a los elementos
+  $card_h2.textContent = name;
+  $card_paragraph.textContent = paragraph;
+  $button_span.textContent = "Más información";
+  
+  //agregar atributos
+  $card_img.setAttribute("src", img);
+  $card_img.setAttribute("alt", name)
+  $button.setAttribute("href","#");
+
+  //agregamos los elementos hijos
+  $card_body.appendChild($card_img);
+  $card_body.appendChild($card_paragraph);
+  $card_header.appendChild($card_h2);
+  $card.appendChild($card_header);
+
+  $button.appendChild($button_span);
+  $button.appendChild($button_icon);
+  $card_body.appendChild($button);
+  $card.appendChild($card_body);
+
+  $fragment.append($card);
+})
+
+$dias.append($fragment);
